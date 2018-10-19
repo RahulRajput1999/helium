2756a15d8070332babe487271f0fbb56b4299dcd
RahulRajput1999 <rahulkumarsindhav@gmail.com>
2018-10-19 23:01:25 +0530

    Added log in and sign up logic

--- routes/index.js
+++ routes/index.js
@@ -17,60 +17,65 @@ router.use(cors());
 router.use(cookieParser());
 router.use(bodyParser.json());
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', {title: 'Express'});
-});
 
-router.get('/login', function (req, res) {
-    res.write(req.query.username);
-    res.end();
-});
 router.post('/login', function (req, res) {
-    console.log(req.body.username);
-    res.write('Posted');
-    res.end();
+    const userInfo = req.body;
+    if (!userInfo.username || !userInfo.password) {
+        res.json({status: false, msg: 'All fields are mercenary'});
+        console.log('l1');
+        res.end();
+    } else {
+        User.find({username: userInfo.username}, function (err, user) {
+            if (user.length <= 0) {
+                res.json({status: false, msg: 'Wrong Username or Password'});
+                res.end();
+            } else{
+                res.json({status: true, msg: 'Successfully logged in', user: user});
+                res.end();
+            }
+        });
+    }
 });
+//User.collection.drop();
 
-router.post('/postuser',function (req,res) {
-   const userInfo = req.body;
-   if(!userInfo.email || !userInfo.username || !userInfo.dob || !userInfo.password) {
-       res.json({status: false, msg: 'All fields are mercenary'});
-       console.log('l1');
-       console.log(userInfo.email + '|' + userInfo.username + '|' + userInfo.dob + '|' + userInfo.password);
-       res.end();
-   } else{
-       let count = 0;
-       User.find({username: userInfo.username},function (err,users){
-           count = users.length;
-       });
-       if(count <= 0) {
-           const newUser = new User({
-               email: userInfo.email,
-               username: userInfo.username,
-               dob: userInfo.dob,
-               password: userInfo.password
-           });
-           newUser.save(function (err, data) {
-               if (err) {
-                   res.json({status: false, msg: 'Something went wrong'});
-                   console.log('l2');
-                   res.end();
-               } else {
-                   res.json({status: true, msg: 'Done'});
-                   res.end();
-               }
-           });
-       } else{
-           res.json({status: false, msg: 'User already exists'});
-           console.log('false-l3');
-           res.end();
-       }
-   }
-});
+router.post('/postuser', function (req, res) {
+    const userInfo = req.body;
+    if (!userInfo.email || !userInfo.username || !userInfo.dob || !userInfo.password) {
+        res.json({status: false, msg: 'All fields are mercenary'});
+        console.log('l1');
+        console.log(userInfo.email + '|' + userInfo.username + '|' + userInfo.dob + '|' + userInfo.password);
+        res.end();
+    } else {
+        let count = 0;
+        User.find({username: userInfo.username}, function (err, users) {
+            count = users.length;
+            console.log(count);
+            if (count <= 0) {
+                const newUser = new User({
+                    email: userInfo.email,
+                    username: userInfo.username,
+                    dob: userInfo.dob,
+                    password: userInfo.password
+                });
+                newUser.save(function (err, data) {
+                    if (err) {
+                        res.json({status: false, msg: 'Something went wrong'});
+                        console.log('l2');
+                        res.end();
+                    } else {
+                        res.json({status: true, msg: 'Done'});
+                        res.end();
+                    }
+                });
+            } else {
+                res.json({status: false, msg: 'User already exists'});
+                console.log('false-l3');
+                res.end();
+            }
+        });
+        console.log('c2' + count);
 
-router.get('/hello', function (req, res) {
-    res.write("Hello");
-    res.end();
+    }
 });
 
 module.exports = router;
